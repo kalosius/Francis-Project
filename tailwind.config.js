83a9f81d82c91f64383ac93dc26b03f247a70907
kalosius <kalosius43@gmail.com>
2026-01-04 20:10:17 +0300

    Color change

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        franciscan: {
+          brown: '#6B4F3F',
+          sand: '#EFE6D8',
+          olive: '#7A8450',
+          charcoal: '#2F2F2F',
+          gold: '#C8A24D',
+        },
+        primary: '#6B4F3F',
+        secondary: '#EFE6D8',
+        accent: '#7A8450',
+        highlight: '#C8A24D',
+      },
+    },
+  },
+};
